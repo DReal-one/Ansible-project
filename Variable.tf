601ee1a962ab65e41a41a91a00d8d6602546def5
Ayobami <ayormmee@gmail.com>
2025-12-13 21:32:40 -0800

    formatted variables

--- Variable.tf
+++ Variable.tf
@@ -1,15 +1,21 @@
 variable "amiid" {
+  description = "AMI ID for the EC2 instance"
   default = "ami-0ecb62995f68bb549"
 }
 variable "instance_type" {
+  description = "Type of EC2 instance"
   default = "t3.micro"
 }
 variable "key_name" {
+  description = "Name of the SSH key pair"
   default = "main"
 }
 variable "region" {
+  description = "AWS region to deploy resources"
   default = "us-east-1"
 }
-variable "severcount" {
-  default = "1"
+variable "servercount" {
+  description = "Number of servers to create"
+  type        = number
+  default     = 1
 }
