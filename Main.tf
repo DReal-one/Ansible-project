c3664c7093dc0a941a8e7b0a714842ad13f88400
Ayobami <ayormmee@gmail.com>
2025-12-13 20:34:49 -0800

    modified main.tf

--- Main.tf
+++ Main.tf
@@ -9,44 +9,44 @@ provider "aws" {
 # CREATE INVENTORY FILE
 #----------------------------------------------------------------
 resource "local_file" "ansible_inventory" {
-  content  = templatefile("templates/hosts.tpl",
-  {
-    key_name      = var.key_name,
-    servers = aws_instance.ansible_nodes.*.public_ip
+  content = templatefile("templates/hosts.tpl",
+    {
+      key_name = var.key_name,
+      servers  = aws_instance.ansible_nodes.*.public_ip
     }
   )
 
-    filename = "./ansible/hosts.cfg"
+  filename = "./ansible/hosts.cfg"
 }
 
 #----------------------------------------------------------------
 # CREATE EC2 ANSIBLE CONTROLLER SERVER
 #----------------------------------------------------------------
 resource "aws_instance" "ansible_controller" {
-  ami           = var.amiid
-  instance_type = var.instance_type
-  key_name      = var.key_name
+  ami                         = var.amiid
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
   associate_public_ip_address = true
 
   tags = {
     Name = "Ansible-Controller"
   }
-  user_data = <<-EOF
+  user_data  = <<-EOF
               #!/bin/bash
               sudo yum update -y
               sudo apt install ansible python3 -y
               EOF
-    depends_on = [aws_instance.ansible_nodes]
+  depends_on = [aws_instance.ansible_nodes]
 }
 
 #----------------------------------------------------------------
 # CREATE ANSIBLE NODES
 #----------------------------------------------------------------
 resource "aws_instance" "ansible_nodes" {
-  count         = var.severcount
-  ami           = var.amiid
-  instance_type = var.instance_type
-  key_name      = var.key_name
+  count                       = var.severcount
+  ami                         = var.amiid
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
   associate_public_ip_address = true
 
   tags = {
@@ -60,15 +60,15 @@ resource "aws_instance" "ansible_nodes" {
 
 
 
-#----------------------------------------------------------------
-# COPY FILES TO ANSIBLE CONTROLLER SERVER
-#----------------------------------------------------------------
-provisioner "file" {
-    source = "./ansible/hosts.cfg"
+  #----------------------------------------------------------------
+  # COPY FILES TO ANSIBLE CONTROLLER SERVER
+  #----------------------------------------------------------------
+  provisioner "file" {
+    source      = "./ansible/hosts.cfg"
     destination = "/home/ubuntu/hosts.cfg"
   }
 
-provisioner "file" {
+  provisioner "file" {
     source      = "./${var.key_name}.pem"
     destination = "/home/ubuntu/${var.key_name}.pem"
   }
@@ -77,17 +77,17 @@ provisioner "file" {
     inline = [
       "chmod 400 /home/ubuntu/${var.key_name}.pem"
     ]
-}
+  }
 
-#----------------------------------------------------------------
-# SETUP SSH CONNECTION TO ANSIBLE CONTROLLER SERVER
-#----------------------------------------------------------------  
+  #----------------------------------------------------------------
+  # SETUP SSH CONNECTION TO ANSIBLE CONTROLLER SERVER
+  #----------------------------------------------------------------  
   connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file("./${var.key_name}.pem")
-      host        = self.public_ip
-    }
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("./${var.key_name}.pem")
+    host        = self.public_ip
+  }
 }
 
 
